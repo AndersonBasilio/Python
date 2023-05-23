22db679f7e314473fb437dc38ffed8086c1cc5de
Anderson Basilio <andersonylde32@gmail.com>
2023-05-22 21:33:24 -0300

    Create pratica14.py
    Exercício

---
+++ pratica14.py
@@ -0,0 +1,21 @@
+
+
+print('+','=' * 15,'+')
+print('|    Exercicio    |')
+print('+','=' * 15, '+')
+
+nome = input('Qual é seu nome? ')
+idade = input('Qual é sua idade? ')
+if nome and idade:
+    print(f'Seu nome é {nome}.')
+    print(f'Seu nome invertido é {nome[::-1]}')
+    if ' ' in nome:
+        print(f'Seu nome tem espaço.')
+    else:
+        print('Seu nome não tem espaços.')
+    print(f'Seu nome tem {len(nome)} letras.')
+    print(f'A primeira letra do seu nome é {nome[:1]}')
+    print(f'A ultima letra do seu nome é {nome[-1]}')
+    print(f'Sua idade é {idade}')
+else:
+    print(f'Desculpe, você deixou campos vazios tente novamente')
