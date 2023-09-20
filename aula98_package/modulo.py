ea0963963a2bf94ee42365e1769e6637e4684954
Anderson Basilio <andersonylde32@gmail.com>
2023-09-20 10:42:19 -0300

    Create modulo.py

---
+++ aula98_package/modulo.py
@@ -0,0 +1,14 @@
+#__all__ = [
+#    'variavel' ,
+#    'nova_variavel',
+#]
+# from aula102_package.modulo_b import fala_oi
+
+variavel = 'alguma coisa'
+
+def soma_do_modulo(x, y):
+    return x + y
+
+nova_variavel = 'Ok'
+
+#fala_oi()
