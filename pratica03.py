3a393a8314cb9a832763319f18d4951a206867c9
Anderson Basilio <andersonylde32@gmail.com>
2023-09-20 10:37:15 -0300

    Create pratica03.py

---
+++ pratica03.py
@@ -0,0 +1,7 @@
+nome = 'Anderson Basilio'
+altura = 1.80
+peso = 95.5
+IMC = peso / (altura * altura)
+
+print(f'\nCalculando o IMC de {nome}.')
+print(f'Olá tudo bem {nome}? sua altura é {altura}, seu peso é {peso} e seu IMC é {IMC:.2f}\n')
