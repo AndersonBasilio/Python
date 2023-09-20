151be32a579b1e50c55fd37509b9007ff2a23943
Anderson Basilio <andersonylde32@gmail.com>
2023-09-20 10:37:11 -0300

    Create pratica02.py

---
+++ pratica02.py
@@ -0,0 +1,32 @@
+a = 10
+b = 5
+print('Operadores Aritmeticos')
+print(f'Adição -> {a} + {b} = {a + b}')
+print(f'Subtração -> {a} - {b} = {a - b}')
+print(f'Multiplicação -> {a} X {b} = {a * b}')
+print(f'Divisão -> {a} / {b} = {a / b}')
+print(f'Divisão Inteira -> {a} // {b} = {a // b}')
+print(f'Exponenciação -> {a} ** {b} = {a ** b}')
+print(f'Modulo -> {a} % {b} = {a % b}')
+print()
+
+print('===' * 15)
+print('Concatenização(+) e Repetição(*)')
+concatenizacao = 'A' + 'B' + 'C'
+a_dez_vezes = 'A' * 10
+tres_vezes_Anderson = 'Anderson / ' * 3
+
+print('Concatenação = ', concatenizacao)
+print('Repetição = ', a_dez_vezes)
+print('Repetição = ', tres_vezes_Anderson)
+print()
+print('===' * 15)
+
+print('Precedências')
+print('1 - (n + n)')
+print('2 - **')
+print('3 - * / // %')
+print('4 - + -')
+print()
+print('===' * 15)
+
