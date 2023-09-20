bba92f16fb30e29031833fffee50ea90cc9f9ad5
Anderson Basilio <andersonylde32@gmail.com>
2023-09-20 10:38:08 -0300

    Create pratica21.py

---
+++ pratica21.py
@@ -0,0 +1,33 @@
+print('while e break')
+"""
+Repetições
+while (Enquanto)
+Executa uma ação enquanto a condição for verdadeira
+Loop infinito -> Quando o código nao tem fim.
+
+while == True
+nome = input('Qual e seu nome: ')
+print(f'Seu nome é {nome}')
+if nome == 'sair':
+    break
+
+print('Acabou.')
+"""
+print('Incremento')
+contador = 0
+
+while contador < 10:
+    contador = contador + 1 # ou assim contador += 1
+    print(contador)
+   
+print('Acabou!') 
+
+print('Decremento')
+contador = 10
+
+while contador >= 0:
+    # ou assim contador -= 1
+    print(contador)
+    contador = contador - 1
+print('Acabou!') 
+
