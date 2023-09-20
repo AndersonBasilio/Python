2db115e8bb5d01bfcf64d2095c557d8bfcb47455
Anderson Basilio <andersonylde32@gmail.com>
2023-09-20 10:41:57 -0300

    Create pratica76.py

---
+++ pratica76.py
@@ -0,0 +1,34 @@
+# Função lambda em Python
+# A função lambda é uma função como qualquer
+# outra em Python. Porém, são funções anônimas
+# que contém apenas uma linha. Ou seja, tudo
+# deve ser contido dentro de uma única expressão
+# lista = [
+#     {'nome': 'Anderson', 'sobrenome': 'Basilio'},
+#     {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+#     {'nome': 'Daniel', 'sobrenome': 'Silva'},
+#     {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+#     {'nome': 'Aline', 'sobrenome': 'Souza'},
+# ]
+# lista = [4, 32, 1, 34, 5, 6, 6, 21, ]
+# lista.sort(reverse=True)
+# sorted(lista)
+
+lista = [
+    {'nome': 'Anderson', 'sobrenome': 'Basilio'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+ ]
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+
+lista1 = sorted(lista, key=lambda item: item['nome'])
+lista2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(lista1)
+exibir(lista2)
