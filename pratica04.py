313b37b761c99191a23bf05eb3dcc37f24a3e1d0
Anderson Basilio <andersonylde32@gmail.com>
2023-09-20 10:37:19 -0300

    Create pratica04.py

---
+++ pratica04.py
@@ -0,0 +1,5 @@
+print('\nFunção "input" para coletar dados do usuário.')
+nome = input('Qual e seu nome? ')
+idade = input('Qual é sua idade? ')
+
+print(f'Ola! Tudo bem?\nSeu nome é {nome} e sua idade é {idade} anos.\n')
