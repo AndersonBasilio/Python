69f1e3cdd2ea6862402830bb58570cb8e2cba337
Anderson Basilio <andersonylde32@gmail.com>
2023-09-20 10:37:08 -0300

    Create pratica01.py

---
+++ pratica01.py
@@ -0,0 +1,33 @@
+print(12, 34)
+print(12, 34, sep=" - ") # Coloca '-' entre os dois numeros.
+print('Anderson', 32, sep=' = idade ')
+
+print('---' * 13)
+
+print('Numeros inteiros (Int)')
+print(-11)
+print(11)
+print(0)
+
+print('---' * 13)
+print('Números com ponto flutante (Float)')
+print(-10.8)
+print(10.8)
+print(0.0)
+print(11.0 , 11.8, sep=' - ' )
+print(type(11.9))
+
+print('---' * 13)
+print('Strings = Texto')
+print("Anderson")
+print('Anderson')
+
+print('---' * 13)
+print('Boolean -> Vai questionar o programa a alguma coisa Se será True ou False')
+print(10 == 10)
+print(10 == 11)
+print(10 < 10.1)
+print(type(True))
+print(type(False))
+print(type(10 == 10))
+print(type (10 == 11))
