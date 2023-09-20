65885966b34f080ebb4e39841ccd93f600e6e887
Anderson Basilio <andersonylde32@gmail.com>
2023-09-20 10:38:12 -0300

    Create pratica22.py

---
+++ pratica22.py
@@ -0,0 +1,9 @@
+"""
+Operadores de atribuição com operadores aritméticos
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+contador = 10
+#contador += 12
+contador /=  2 # Retornará em float 
+print(contador)
