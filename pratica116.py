f994ebe029756d010a6d905e6a6c7909fe5b8ee8
Anderson Basilio <andersonylde32@gmail.com>
2023-10-18 14:48:11 -0300

    Create pratica116.py
    Ambientes virtuais em Python (venv)

---
+++ pratica116.py
@@ -0,0 +1,17 @@
+# Ambientes virtuais em Python (venv)
+# Um ambiente virtual carrega toda sua instalação 
+# do Python para uma pasta no caminho escolhido
+# Ao ativar um ambiente virtual, a instalação do
+# ambiente virtual será usada.
+# venv é um módulo que vamos usar para
+# criar ambientes virtuais.
+# Você pode dar o nome que preferir para um
+# ambiente virtual, mas os mais comuns são:
+# venv, env, .venv .env
+#
+# Como criar ambientes virtuais
+# Abra a pasta do seu projeto no terminal
+# e digite:
+# python -m venv venv
+import pymysql
+print('Oi')
